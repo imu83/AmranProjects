bdb6dd6dd076b00004189ff967dba11ac94cb54d
imu83 <imu786@gmail.com>
2015-01-11 16:39:35 +0600

    commit 2
    second change was made

--- application/BabulVai/scripts/validation.js
+++ application/BabulVai/scripts/validation.js
@@ -1,1 +1,21 @@
-@CHARSET "ISO-8859-1";
+function IsValid()
+{
+	var isvalid = false;	
+
+	var userID =document.getElementById('userID');
+	var password =document.getElementById('password');
+		
+	if(userID.value!='')
+	{		
+		isvalid = true;
+		}
+	if(password.value!='')
+	{
+		isvalid = true;
+		}
+	else {		
+		alert('please fill the mandatory');
+		isvalid = false;
+	}
+	return isvalid;
+}
