f8ea117f2ad1a3e5e90e87c122a4423fe1dd14a3
imu83 <imu786@gmail.com>
2015-01-14 16:11:27 +0600

    created a CWHW folder
    created a CWHW folder for adding only service files to the repo.

--- application/BabulVai/UpdateProfile.php
+++ application/BabulVai/UpdateProfile.php
@@ -3,9 +3,12 @@ include_once 'Utility/Functions.php';
 
 $parameterList = array (
 		"uid",
-		"uname",
-		"umobile",
+		"upass",
+		"ufullname",		
 		"uaddress",
+		"ucity",
+		"ucountry",
+		"ucell",		
 		"uemail" 
 );
 
@@ -15,15 +18,25 @@ if (sizeof ( $validList ) == sizeof ( $parameterList )) {
 	SetupConnectionToDB ();
 	$db_tbl_name = 'profiles';
 	
-	$myQuery = "update {$db_tbl_name}
-	set userName = '{$validList [1]}',
-	userMobile = '{$validList [2]}', 
+	$myQuery = "update {$db_tbl_name} set
+	userPassword = '{$validList [1]}',
+	userFullName = '{$validList [2]}',	 
 	userAddress = '{$validList [3]}',
-	userEmail = '{$validList [4]}'
-	
+	userCity = '{$validList [4]}',
+	userCountry = '{$validList [5]}', 
+	userCell = '{$validList [6]}',
+	userEmail = '{$validList [7]}'
 	where userID = '{$validList [0]}'";
 	
 	$resultSet = mysql_query ( $myQuery ) or die ( PrintAsJsonFailedWithMessage ( mysql_error () ) );
+	
+	if ($resultSet) {
+		$myQuery = "update users set 
+		userPassword = '{$validList [1]}'  
+		where userID = '{$validList [0]}'";
+	
+		$resultSet = mysql_query ( $myQuery ) or die ( PrintAsJsonFailedWithMessage ( mysql_error () ) );
+	}
 	
 	$myQuery = "SELECT * FROM {$db_tbl_name} where userID = '{$validList [0]}'";
 	$resultSet = mysql_query ( $myQuery ) or die ( PrintAsJsonFailedWithMessage ( mysql_error () ) );
